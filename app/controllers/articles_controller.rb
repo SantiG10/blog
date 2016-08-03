60e85aa8c6601ee30978a0610f2386d23cfd070f
Santiago Giraldo <santi_rm8@hotmail.com>
2016-08-03 15:42:35 -0500

    algunas modificaciones

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
-	
+	before_action :authenticate_user!, except: [:show,:index]
+	before_action :set_article, except: [:index,:new,:create]
 	#GET /articles
 	def index
 		@articles = Article.all
 	end	
 	#GET /articles/:id
 	def show
-		@article = Article.find(params[:id])
+		@article.update_visits_count
+		@comment = Comment.new
 	end	
 	#GET /articles/new
 	def new 
@@ -14,11 +16,10 @@ class ArticlesController < ApplicationController
 	end	
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 	#POST /article/new
 	def create
-		@article = Article.new(article_params)
+		@article = current_user.articles.new(article_params)
 		if @article.save
 			redirect_to @article
 		else
@@ -27,7 +28,6 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
 	end
@@ -35,7 +35,6 @@ class ArticlesController < ApplicationController
 
 	#PUT /articles/:id
 	def update
-		@article = Article.find(params[:id])
 		if @article.update(article_params)
 			redirect_to @article
 		else
@@ -45,8 +44,12 @@ class ArticlesController < ApplicationController
 
 	private
 
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
 	def article_params
-		params.require(:article).permit(:title,:body)
+		params.require(:article).permit(:title,:body,:cover)
 	end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,20 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, uniqueness: true
-	validates :body, presence: true, length: {minimum: 20}
+  belongs_to :user
+  has_many :comments
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true, length: {minimum: 20}
+  before_create :set_visits_count
+
+  has_attached_file :cover, styles: { medium: "1280x720", thumb:"800x600"}
+  validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
+
+  def update_visits_count
+    self.update(visits_count: self.visits_count + 1)
+  end
+
+  private
+
+  def set_visits_count
+    self.visits_count = 0
+  end
 end
